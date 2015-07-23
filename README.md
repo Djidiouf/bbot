88cefd9c9ce250a4cd1d5c65e3afe0da9f7b6c5e
Djidiouf <djidiouf@gmail.com>
2015-07-24 00:34:40 +1000

    ADD bbot init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # bbot
 bbot is an advanced IRC bot with fancy answers and features
+
+## Installation
+Platforms: Windows, MacOSX, Linux
+
+Prerequisite: [Python 3.4](https://www.python.org/)
+
+Download bbot by clicking on [Download ZIP](https://github.com/Djidiouf/bbot/archive/master.zip), or do a git clone.
+
+Extract the bbot folder on your desktop and open a command line terminal (Windows: shift + right click on your desktop and do 'Open command window here').
+
+```sh
+$ cd bbot
+$ python bbot.py -s <SERVER>  -c <CHANNEL> -b <BOTNICKNAME>
+```
+
+### Version
+0.1
+
+### Tech
+bbot is made in:
+
+* Python - 3.4.3
+
+### Development
+Want to contribute? Great! But don't do it now, wait for the Release.
+
+
+### Todo's
+* more features
+* other things
+
+### License
+Something
---
+++ bbot.py
@@ -0,0 +1,69 @@
+# -------------------------------------------------------------------------------
+# Name:         bbot
+# Purpose:      a bot without limits
+#
+# Author:       Djidiouf
+#
+# Created:      2015-07-23
+# Licence:      bchat-licence
+# -------------------------------------------------------------------------------
+
+# Import socket library
+import socket
+# Import argument library
+import argparse
+
+
+# functions ---------------------------------------------------------------------
+# functions that will do the handling of the servers's data
+
+
+def ping():  # Respond to server pings
+    ircsock.send(bytes("PONG :Pong\n", "UTF-8"))
+
+
+def join_chan(chan):  # Join channel
+    ircsock.send(bytes("JOIN %s\r\n" % chan, "UTF-8"))
+
+
+def hello():  # Responds to a user that inputs "Hello <botname>"
+    ircsock.send(bytes("PRIVMSG %s :Hello \r\n" % args.channel, "UTF-8"))
+
+
+# arguments ---------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='bbot, a bot without limits')
+parser.add_argument("-s", "--server", help="Server name", required=True)
+parser.add_argument("-c", "--channel", help="Channel name", required=True)
+parser.add_argument("-b", "--botnick", help="bbot nickname", required=True)
+args = parser.parse_args()
+
+
+# connection --------------------------------------------------------------------
+# connect and join the configured channel
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ircsock.connect((args.server, 6667))  # Connect to port 6667
+
+# Sends the username, real name etc : user authentication
+ircsock.send(bytes("USER %s %s aaa :%s\r\n" % (args.botnick, args.botnick, args.botnick), "UTF-8"))
+
+# Assignment of a nick to the bot
+ircsock.send(bytes("NICK %s\r\n" % args.botnick, "UTF-8"))
+
+# After connection, oin the specified channel
+join_chan(args.channel)
+
+
+# data reception ---------------------------------------------------------------
+# Receive all the data from the server & channel
+while 1:  # infinite loop
+    ircmsg = ircsock.recv(2048)  # receive data from the server
+    ircmsg = ircmsg.strip(bytes("\n\r", "UTF-8"))  # removes unnecessary linebreaks
+    print(ircmsg)  # DEBUG: print output of the channel
+
+    # tracks "Hello <botname>"
+    if ircmsg.find(bytes(":Hello %s" % args.botnick, "UTF-8")) != -1:
+        hello()
+
+    # tracks PING : if the server pings the bot, it will answer
+    if ircmsg.find(bytes("PING :", "UTF-8")) != -1:
+        ping()
