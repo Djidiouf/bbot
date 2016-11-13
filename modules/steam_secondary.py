14636fa9c3429758c85cd7b3bd9706e573cc4570
Djidiouf <djidiouf@gmail.com>
2016-11-14 01:59:49 +1100

    switch to requests library for steam_secondary modules

--- modules/steam_secondary.py
+++ modules/steam_secondary.py
@@ -1,7 +1,7 @@
 __author__ = 'Djidiouf'
 
 # Python built-in modules
-import urllib.request  # Open url request on website
+import requests
 
 # Third party modules
 from bs4 import BeautifulSoup
@@ -13,19 +13,18 @@ import modules.textalteration
 
 def get_russian_price(i_string):
 
-    title_requested=modules.textalteration.string_replace(i_string, " ", "+")
+    title_requested = modules.textalteration.string_replace(i_string, " ", "+")
 
     url = "http://www.allkeyshop.com/catalogue/search.php?q=%s&sort=nameAsc" % title_requested
-    webpage = urllib.request.urlopen(url)
-    # soup = BeautifulSoup(webpage.read(), 'html.parser')
-    soup = BeautifulSoup(webpage.read(), 'lxml')
+    webpage = requests.get(url)
+    soup = BeautifulSoup(webpage.text, 'lxml')
 
-    #search_results = soup.select(".searchresults #table1 #tbody1 #tr4 #td5 #strong2")
-    url = soup.select(".searchresults table tr:nth-of-type(2) td:nth-of-type(1) a")
+    # search_results = soup.select(".searchresults #table1 #tbody1 #tr4 #td5 #strong2")
+    game_url = soup.select(".searchresults table tr:nth-of-type(2) td:nth-of-type(1) a")
     price = soup.select(".searchresults tr:nth-of-type(2) strong")
 
-    store_url=url[0].get('href')
-    store_price=price[0].get_text()
-    tup_russian=(store_url, store_price)
+    store_url = game_url[0].get('href')
+    store_price = price[0].get_text()
+    tup_russian = (store_url, store_price)
 
     return tup_russian
