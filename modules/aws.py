c9c308c83bf71601beef2b13e4ad95ac8b267eed
Djidiouf <djidiouf@gmail.com>
2017-10-17 23:12:38 +1100

    add !aws command

---
+++ modules/aws.py
@@ -0,0 +1,95 @@
+__author__ = 'Djidiouf'
+
+# Python built-in modules
+import configparser
+import os
+import json
+
+# Third-party modules
+import boto3
+from botocore.exceptions import ClientError
+
+# Project modules
+import modules.connection
+
+
+class MyExceptionAWSInProgress(Exception):
+    """Raise for my specific kind of exception"""
+    pass
+
+def lambda_run(i_args):
+    results = "Not implemented"
+    return results
+
+
+def asg_desired_capacity(i_string):
+    tuple_string = i_string.partition(' ')
+    requested_server = tuple_string[0]
+    requested_capacity = tuple_string[2]
+
+    # Read config
+    config = configparser.ConfigParser()
+    config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'config.cfg'))  # Absolute path is better
+
+    # Establish real server name
+    if requested_server == "teamspeak":
+        requested_server = config['aws']['teamspeak_asg']
+    else:
+        results = "Error: The value %s is invalid" % (requested_server)
+        return results
+
+    # Establish requested capacity
+    if requested_capacity == 'on' or requested_capacity == 'up' or requested_capacity == '1':
+        requested_capacity = 1
+    elif requested_capacity == 'off' or requested_capacity == 'down' or requested_capacity == '0':
+        requested_capacity = 0
+    else:
+        results = "Error: The value %s is invalid" % (requested_capacity)
+        return results
+
+    # Run command
+    client = boto3.client('autoscaling')
+    try:
+        results = client.set_desired_capacity(
+            AutoScalingGroupName=requested_server,
+            DesiredCapacity=requested_capacity,
+            HonorCooldown=True,
+        )
+        if results['ResponseMetadata']['HTTPStatusCode'] == 200:
+            results = "A request to change the %s desired capacity to %s has been issued" % (requested_server, requested_capacity)
+            return results
+        else:
+            results = "Error: Unexpected"
+            return results
+
+    except ClientError as e:
+        print(e.response['Error']['Code'])
+        if e.response['Error']['Code'] == 'ScalingActivityInProgress':
+            raise MyExceptionAWSInProgress("A Scaling activity is already in progress for %s" % requested_server)
+        else:
+            raise e
+
+
+def aws(i_string):
+    antispam_timer = 0  # in sec
+
+    tuple_string = i_string.partition(' ')
+    sub_cmd = tuple_string[0]
+    sub_arg = tuple_string[2]
+
+    if sub_cmd == "asg":
+        aws_results = asg_desired_capacity(sub_arg)
+        return aws_results
+    elif sub_cmd == "lambda":
+        aws_results = lambda_run(sub_arg)
+        return aws_results
+
+
+def main(i_string, i_medium, i_alias=None):
+    try:
+        aws_results = aws(i_string.lower)
+        modules.connection.send_message("%s" % aws_results, i_medium, i_alias)
+
+    except MyExceptionAWSInProgress as e:
+        modules.connection.send_message("%s" % e.args, i_medium, i_alias)
+        return
--- modules/help.py
+++ modules/help.py
@@ -7,17 +7,25 @@ import modules.connection
 def print_help_help(i_medium=None, i_alias=None):
     modules.connection.send_message("Usage: !help <handler>", i_medium, i_alias)
     modules.connection.send_message("Known handlers:", i_medium, i_alias)
-    modules.connection.send_message("!calc !imdb !meet !money !ping !quit !say !steam !time !yt", i_medium, i_alias)
+    modules.connection.send_message("!aws !calc !imdb !meet !money !ping !quit !say !steam !time !yt", i_medium, i_alias)
 
 
 def display_help(i_string, mode, i_medium=None, i_alias=None):
     if i_string == "!help":
         print_help_help(i_medium, i_alias)
+
+    elif i_string == "!aws":
+        modules.connection.send_message("Usage: !aws asg <ASG> <Status/Desired Capacity>", i_medium, i_alias)
+        modules.connection.send_message("Usage: !aws lambda <Function Name>", i_medium, i_alias)
+        if mode == "detailed":
+            modules.connection.send_message("Purpose: Issue AWS commands", i_medium, i_alias)
+
     elif i_string == "!calc":
         modules.connection.send_message("Usage: !calc <Operations>", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Compute things faster than you", i_medium, i_alias)
             modules.connection.send_message("Tip: Functions available > https://docs.python.org/3/library/math.html", i_medium, i_alias)
+
     elif i_string == "!imdb":
         modules.connection.send_message("Usage: !imdb <Guessed Title>", i_medium, i_alias)
         modules.connection.send_message("Usage: !imdb <Guessed Title>#<Year>", i_medium, i_alias)
@@ -25,31 +33,37 @@ def display_help(i_string, mode, i_medium=None, i_alias=None):
         if mode == "detailed":
             modules.connection.send_message("Purpose: Give information about a movie / TV serie", i_medium, i_alias)
             modules.connection.send_message("Tip: Title you enter is a guessed title, specify the year for more accuracy", i_medium, i_alias)
+
     elif i_string == "!meet":
         modules.connection.send_message("Usage: !meet <TimeZone> <HH:MM>", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Give the equivalence of the specified time input in several time zones", i_medium, i_alias)
             modules.connection.send_message("Tip: Valid time zones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones", i_medium, i_alias)
+
     elif i_string == "!money":
         modules.connection.send_message("Usage: !money <number> <CODE1>:<CODE2>", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Convert an amount from one currency to another", i_medium, i_alias)
             modules.connection.send_message("Tip: Valid currency codes: https://en.wikipedia.org/wiki/ISO_4217", i_medium, i_alias)
+
     elif i_string == "!ping":
         modules.connection.send_message("Usage: !ping <optional_ip>", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Make me ping either you or a given IP/DNS if possible", i_medium, i_alias)
             modules.connection.send_message("Tip: Works for IPv4 or IPv6", i_medium, i_alias)
+
     elif i_string == "!quit":
         modules.connection.send_message("Usage: !quit", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Make me quit IRC", i_medium, i_alias)
             modules.connection.send_message("Tip: Don't be mean please :'(", i_medium, i_alias)
+
     elif i_string == "!say":
         modules.connection.send_message("Usage: !say <something>", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Make me speak!", i_medium, i_alias)
             modules.connection.send_message("Tip: Be smart and /msg me the command privately", i_medium, i_alias)
+
     elif i_string == "!steam":
         modules.connection.send_message("Usage: !steam <Game_Title>", i_medium, i_alias)
         modules.connection.send_message("Usage: !steam admin <Admin_Command>", i_medium, i_alias)
@@ -57,12 +71,14 @@ def display_help(i_string, mode, i_medium=None, i_alias=None):
         modules.connection.send_message("Usage: !steam played <Game_Title>", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Expose Steam information", i_medium, i_alias)
+
     elif i_string == "!time":
         modules.connection.send_message("Usage: !time <TimeZone>", i_medium, i_alias)
         if mode == "detailed":
             modules.connection.send_message("Purpose: Give the time in the specified time zone", i_medium, i_alias)
             modules.connection.send_message("Tip: Get valid timezones of a country with: !time <two-letter-country-code>", i_medium, i_alias)
             modules.connection.send_message("---- Countries' codes: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2", i_medium, i_alias)
+
     elif i_string == "!yt":
         modules.connection.send_message("Usage: !yt <Display name or ID>", i_medium, i_alias)
         if mode == "detailed":
